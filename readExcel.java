8836e56d37e48704fadd256d2b21d8a736d2fcac
pcamposr <31778161+pcamposr@users.noreply.github.com>
2017-09-27 22:25:29 -0300

    Update readExcel.java
    código funcionando, se debe arreglar las variables y buscar simplificarlo.

--- readExcel.java
+++ readExcel.java
@@ -3,37 +3,68 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-// VERSION 0.1 
-package readexcel;
 
+// agregar los 6 archivos .jar correspondientes para que funcione el codigo 
+//archivos .jar : https://drive.google.com/file/d/0B1T79UjkY81wTFBCcm5kbTNrYjQ/view 
+package leer.archivos.de.excel;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 /**
- *
+ * 
  * @author pablo
  */
-import java.io.File;
-import java.io.IOException;
-
-import jxl.*;
-import jxl.read.biff.BiffException;
-
-public class readExcel {
+public class LeerArchivosDeExcel {
+    public LeerArchivosDeExcel(File filename){
+        List cellData = new ArrayList() ;
+        try { 
+            
+            FileInputStream fileInputStream = new FileInputStream( filename);
+            XSSFWorkbook workBook = new XSSFWorkbook(fileInputStream);
+            XSSFSheet hssfSheet = workBook.getSheetAt(0);
+            Iterator rowIterator = hssfSheet.rowIterator();
+            while(rowIterator.hasNext()){
+                XSSFRow hssfRow = (XSSFRow) rowIterator.next();
+                Iterator iterator = hssfRow.cellIterator();
+                List cellTemp = new ArrayList();
+                    while (iterator.hasNext()){
+                        XSSFCell hssfcell = (XSSFCell) iterator.next();
+                        cellTemp.add(hssfcell);
+                }
+                cellData.add(cellTemp);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        obtener(cellData);
+    }
+    
     /**
      * @param args the command line arguments
      */
-
-public static void main(String[] args) throws IOException, BiffException {
-Workbook workbook = Workbook.getWorkbook(new File(“C:/Users/pablo/Documents/prueba.xlsx”)); // <--- arreglar este detalle
-Sheet sheet = workbook.getSheet(0); 
-String nombre;
-
-for (int fila = 1; fila < sheet.getRows(); fila++) { 
-for (int columna = 0; columna < sheet.getColumns(); columna++) { 
-nombre = sheet.getCell(columna, fila).getContents(); 
-System.out.print(nombre + ""); 
-
-}
-System.out.println("\n");
-System.out.println("--------------------");
-}
-}
+    private void obtener(List cellDataList){
+        for (int i = 0 ; i < cellDataList.size() ; i++){   
+    List cellTempList = (List) cellDataList.get(i);
+        for (int j = 0 ; j < cellTempList.size(); j++){
+         XSSFCell hssfCell = (XSSFCell) cellTempList.get(j);
+         String strinCellValue = hssfCell.toString();
+         System.out.print(strinCellValue+" ");
+        }
+        System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        File f=new File("C:/Users/Pablo/Desktop/prueba.xlsx");
+        if (f.exists()) { 
+            LeerArchivosDeExcel obj = new LeerArchivosDeExcel(f);
+        }
+    }
+    
 }
