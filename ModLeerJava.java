922e0888c872c020a008f963a35d3828e0080d80
pcamposr <31778161+pcamposr@users.noreply.github.com>
2017-11-14 12:47:11 -0300

    Update ModLeerJava.java

--- ModLeerJava.java
+++ ModLeerJava.java
@@ -1,6 +1,7 @@
 
 package modleerjava;
 
+
 import java.io.IOException;
 
 /* bajar archivos .jar desde http://www-us.apache.org/dist/poi/release/bin/poi-bin-3.17-20170915.zip  */ 
@@ -10,11 +11,10 @@ public class ModLeerJava {
 
     public static void main(String[] args) throws IOException {
         leerExcel objExcel = new leerExcel(); 
-        String rutaFile = System.getProperty("user.dir")+"C:/Users/Pablo/Desktop/prueba.xlsx"; 
-        objExcel.readExcel(rutaFile, "prueba.xlsx", "Sheet1");
-        
+        String rutaExcel = System.getProperty("user.dir")+"C:/Users/Pablo/Desktop/TAREA/prueba.xlsx"; 
+        objExcel.readExcel(rutaExcel, "prueba.xlsx", "Sheet1");
         modExcel excelObj = new modExcel(); 
-        excelObj.modCelda(1, 2, "asd", "C:/Users/Pablo/Desktop/prueba.xlsx");
+        excelObj.modCelda(1, 2, "asd", "C:/Users/Pablo/Desktop/TAREA/prueba.xlsx");
     }
     
 }
